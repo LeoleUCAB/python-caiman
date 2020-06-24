92d2d9efb278604e334e0528c5e135ae61c0bb8c
Alfonso Navarro <alfonsonavarromedina@gmail.com>
2020-06-24 13:54:49 -0400

    Arreglado el error de insercion en la DB

--- dataBase.py
+++ dataBase.py
@@ -17,7 +17,7 @@ sql_create_ingredient_table = """ CREATE TABLE IF NOT EXISTS ingredient (
                                         id integer,
                                         size_id integer,
                                         name text not null,
-                                        price   real not null,
+                                        price real not null,
                                         constraint ingredient_pk primary key (id, size_id)
                                     ); """
 
@@ -30,17 +30,16 @@ sql_create_size_table = """ CREATE TABLE IF NOT EXISTS size (
 # INSERTS -------------------------------------------------------------------------------------------------------
 
 sql_insert_pizza_table = """ """
+sql_insert_size_table_0 = """INSERT INTO size (id, name, price) VALUES (1, 'Personal', 10)"""
+sql_insert_size_table_1 = """INSERT INTO size (id, name, price) VALUES (2, 'Mediana', 15)"""
+sql_insert_size_table_2 = """INSERT INTO size (id, name, price) VALUES (3, 'Familiar', 20)"""
 
-sql_insert_size_table_0 = """INSERT INTO size (id, name, price) VALUES (1, 'Personal', 10);"""
-sql_insert_size_table_1 = """INSERT INTO size (id, name, price) VALUES (2, 'Mediana', 15);"""
-sql_insert_size_table_2 = """INSERT INTO size (id, name, price) VALUES (3, 'Familiar', 20);"""
-
-sql_insert_ingredient_table_0 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (1, 1, 'Champiñones', 1.75);"""
-sql_insert_ingredient_table_1 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (1, 2, 'Champiñones', 2.05);"""
-sql_insert_ingredient_table_2 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (1, 3, 'Champiñones', 2.5);"""
-sql_insert_ingredient_table_3 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (2, 1, 'Jamón', 1.5);"""
-sql_insert_ingredient_table_4 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (2, 2, 'Jamón', 1.75);"""
-sql_insert_ingredient_table_5 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (2, 3, 'Jamón', 2);"""
+sql_insert_ingredient_table_0 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (1, 1, 'Champiñones', 1.75)"""
+sql_insert_ingredient_table_1 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (1, 2, 'Champiñones', 2.05)"""
+sql_insert_ingredient_table_2 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (1, 3, 'Champiñones', 2.5)"""
+sql_insert_ingredient_table_3 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (2, 1, 'Jamón', 1.5)"""
+sql_insert_ingredient_table_4 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (2, 2, 'Jamón', 1.75)"""
+sql_insert_ingredient_table_5 = """INSERT INTO ingredient (id, size_id, name, price) VALUES (2, 3, 'Jamón', 2)"""
 
 def create_connection(db_file):
     """creating DataBase connection"""
@@ -70,6 +69,7 @@ def insert_table(connection, insert_table_sql, nombreTabla):
         cursor = connection.cursor()
         cursor.execute(insert_table_sql)
         print(f'Insertados datos en la tabla: {nombreTabla}')
+        connection.commit()
     except:
         print(f'Ha ocurrido un error al insertar los datos en la tabla: {nombreTabla}')
 
@@ -79,12 +79,12 @@ if __name__ == '__main__':
 
     # create tables
     if connection is not None:
-        # create pizza table
-        create_table(connection, sql_create_pizza_table, "Pizza")
-        # create ingrediente table
-        create_table(connection, sql_create_ingredient_table, "Ingrediente")
         # create size table
         create_table(connection, sql_create_size_table, "Tamaño")
+        # create ingredient table
+        create_table(connection, sql_create_ingredient_table, "Ingrediente")
+        # create pizza table
+        create_table(connection, sql_create_pizza_table, "Pizza")
 
         #insert in table size
         insert_table(connection, sql_insert_size_table_0, "Tamaño 0")
